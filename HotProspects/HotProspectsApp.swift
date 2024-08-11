046e3150a878193ec23c257f04cea34ccce4d5d3
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-08-10 23:49:04 -0300

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
